7c2fc20f33de070039a819afa7f11a1fe4ab1d72
yatinnagpal <yatinnagpal2016@gmail.com>
2025-04-05 11:52:21 +0530

    implement GET endpoint for fetching tasks with pagination

--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,4 @@
-from fastapi import Depends
+from fastapi import Depends, Query
 from . import schemas, models
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -10,3 +10,8 @@ def create_task(request:schemas.Task, db : Session = Depends(get_db)):
     db.refresh(new_task)
     print(db.query(models.Task).all())
     return new_task
+
+
+def get_task(limit: int = Query(10, ge=0), skip: int = Query(0, ge=0), db: Session = Depends(get_db)):
+    task = db.query(models.Task).offset(skip).limit(limit).all()
+    return task
--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,8 @@ models.Base.metadata.create_all(bind=engine)
 @router.post("/create_task", response_model=schemas.Task, status_code=201)
 def create_task(request:schemas.Task, db : Session = Depends(get_db)):
     return crud.create_task(request, db)
+
+
+@router.get("/get_task", response_model=list[schemas.Task])
+def get_tasks(limit: int = 10, skip: int = 0, db: Session = Depends(get_db)):
+    return crud.get_task(limit, skip, db)
